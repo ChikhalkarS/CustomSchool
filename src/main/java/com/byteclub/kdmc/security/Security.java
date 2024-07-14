9201ab06d479e5ec3e957b1929fa8cfed51b5606
ChikhalkarS <Shubhamchikhalkar25@gmail.com>
2024-07-14 17:09:14 +0530

    #JWT [Shubham] Intoduce JSON WEB TOKEN and Added Insomnia Collection

--- src/main/java/com/byteclub/kdmc/security/Security.java
+++ src/main/java/com/byteclub/kdmc/security/Security.java
@@ -2,10 +2,25 @@ package com.byteclub.kdmc.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class Security {
+
+    @Order(1)
+    @Bean
+    public SecurityFilterChain filteringCriteria(HttpSecurity http) throws Exception {
+        http.cors().disable();
+        http.csrf().disable();
+//        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/*").permitAll());
+//        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/product/*").authenticated());
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        return http.build();
+    }
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
--- src/main/java/com/byteclub/kdmc/service/AuthService.java
+++ src/main/java/com/byteclub/kdmc/service/AuthService.java
@@ -13,16 +13,19 @@ import com.byteclub.kdmc.model.SessionStatus;
 import com.byteclub.kdmc.model.Student;
 import com.byteclub.kdmc.repository.SessionRepository;
 import com.byteclub.kdmc.repository.StudentRepository;
-import org.apache.commons.lang3.RandomStringUtils;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMapAdapter;
-
+import javax.crypto.SecretKey;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -73,7 +76,24 @@ public class AuthService {
     }
 
     private String createSession(Optional<Student> student) {
-        String token = RandomStringUtils.randomAlphanumeric(40);
+        //token generation
+        //String token = RandomStringUtils.randomAlphanumeric(30);
+        MacAlgorithm alg = Jwts.SIG.HS256; // HS256 algo added for JWT
+        SecretKey key = alg.key().build(); // generating the secret key
+
+        //start adding the claims
+        Map<String, Object> jsonForJWT = new HashMap<>();
+        jsonForJWT.put("userId", student.get().getEmail());
+        //jsonForJWT.put("roles", student.get().s.getRoles());
+        jsonForJWT.put("createdAt", new Date());
+        jsonForJWT.put("expiryAt", new Date(LocalDate.now().plusDays(3).toEpochDay()));
+
+        String token = Jwts.builder()
+                .claims(jsonForJWT) // added the claims
+                .signWith(key, alg) // added the algo and key
+                .compact(); //building the token
+
+        //String token = RandomStringUtils.randomAlphanumeric(40);
         Session session = new Session(token, new Date(), new Date(), student.get(), SessionStatus.ACTIVE);
 
         sessionRepository.save(session);
@@ -83,7 +103,7 @@ public class AuthService {
 
     public ResponseEntity<Void> logout(String token) {
         Optional<Session> session = sessionRepository.findByToken(token);
-        if(session.isEmpty())
+        if(session.isEmpty() || SessionStatus.ENDED.equals(session.get().getSessionStatus()))
         {
             throw  new InvalidUserSessionException("Invalid Session");
         }
